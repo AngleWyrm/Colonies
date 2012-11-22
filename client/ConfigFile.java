082415cc68d92dbf8ce99d45c50887c2b436618a
lohikaarme <lohikaarme.hav@kolumbus.fi>
2012-11-22 22:37:04 +0200

    Update client/ConfigFile.java
    Made so that file is created and changes how root is find.I shorted 
    files name to the Colony since that is now the name :D
    Colony.cfg is created at jar folder. MCP folder doesn't exist at 
    Minecraft's folder system. ".minecraft" = "jars". Files will 
    automatically go there. No need for root handling. 
    I didn't have time to make config folder.

--- client/ConfigFile.java
+++ client/ConfigFile.java
@@ -1,17 +1,73 @@
-// Create, read and write configuration file
-// stored in user.dir/../config/MineColony.cfg
+// 
+// data is in a public static hash table called ConfigFile.settings
+//   ConfigFile.settings.getProperty("myNewBlockID");
+//   ConfigFile.settings.setProperty("myNewBlockID", "1500");
+//
+// config file location is ../config/MineColony.cfg
+//
 package client;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import net.minecraft.client.Minecraft;
 
 public class ConfigFile {
 
-	public static void CreateDefaultConfig() {
-			String configFilePath;
-			configFilePath = File.separator +".." + File.separator + "config";
-			// test for and possibly create config folder
-			// test for and possibly create MineColony.cfg
-			// catch exceptions and post to stream
-			// failure to create means use defaults in-game
+	public static Properties settings = new Properties();
+	private static String configFilePath="Colony.cfg";
+
+	public static void createDefaultConfiguration() {
+	   // Default key/value pairs in ConfigFile.settings
+	   settings.setProperty("MineColony", "Reboot");
+
+	   //test purposes
+	   settings.setProperty("townhallID","1000");
+	   //
+
+           save();
 	}
+
+
+	public static void load() {
+	    File configFile = new File(configFilePath);
+
+	    try{
+    	      if(configFile.exists() ){
+    	        FileInputStream in = new FileInputStream(configFile);	
+    	        settings.load(in);
+    	      }
+    	      else {
+    	        createDefaultConfiguration();
+   	      }
+
+	   }
+	   catch (Exception e) {
+             System.err.println("[MineColony] " + e.getMessage());
+             System.err.flush();
+           }
+         }
+
+
+         public static void save() {
+           File configFile = new File(configFilePath);
+           try{
+             configFile.createNewFile();
+             FileOutputStream out = new FileOutputStream(configFile);
+             settings.store(out, "MineColony configuration file");
+             load();
+           }
+           catch (FileNotFoundException e) {
+             System.err.println("[MineColony] " + e.getMessage()+" save,1");
+             System.err.flush();
+           }
+           catch (IOException e){
+             System.err.println("[MineColony] " + e.getMessage()+" save,2");
+             System.err.flush();
+           }
+         }
 }
