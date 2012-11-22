5129557a6c35932d1bd17d417d9a2756f6842385
lohikaarme <lohikaarme.hav@kolumbus.fi>
2012-11-22 22:38:31 +0200

    Update client/ClientProxy.java

--- client/ClientProxy.java
+++ client/ClientProxy.java
@@ -1,73 +1,28 @@
-// 
-// data is in a public static hash table called ConfigFile.settings
-//   ConfigFile.settings.getProperty("myNewBlockID");
-//   ConfigFile.settings.setProperty("myNewBlockID", "1500");
-//
-// config file location is ../config/MineColony.cfg
-//
 package client;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
+import net.minecraft.src.World;
+import net.minecraftforge.client.MinecraftForgeClient;
+import server.ServerProxy;
+import cpw.mods.fml.client.FMLClientHandler;
 
-import net.minecraft.client.Minecraft;
+public class ClientProxy extends ServerProxy
+{
 
-public class ConfigFile {
-	
-	public static Properties settings = new Properties();
-	private static String configFilePath="Colony.cfg";
-			
-	public static void createDefaultConfiguration() {
-	   // Default key/value pairs in ConfigFile.settings
-	   settings.setProperty("MineColony", "Reboot");
+	@Override
+    public void registerRenderInformation()
+    {
+        MinecraftForgeClient.preloadTexture("/minecolony.png");
+    }
 
-	   //test purposes
-	   settings.setProperty("townhallID","1000");
-	   //
+    @Override
+    public void registerTileEntitySpecialRenderer(/*PLACEHOLDER*/)
+    {
+       
+    }
 
-           save();
-	}
-	
-	
-	public static void load() {
-	    File configFile = new File(configFilePath);
-	  
-	    try{
-    	      if(configFile.exists() ){
-    	        FileInputStream in = new FileInputStream(configFile);	
-    	        settings.load(in);
-    	      }
-    	      else {
-    	        createDefaultConfiguration();
-   	      }
-
-	   }
-	   catch (Exception e) {
-             System.err.println("[MineColony] " + e.getMessage());
-             System.err.flush();
-           }
-         }
-
-	
-         public static void save() {
-           File configFile = new File(configFilePath);
-           try{
-             configFile.createNewFile();
-             FileOutputStream out = new FileOutputStream(configFile);
-             settings.store(out, "MineColony configuration file");
-             load();
-           }
-           catch (FileNotFoundException e) {
-             System.err.println("[MineColony] " + e.getMessage()+" save,1");
-             System.err.flush();
-           }
-           catch (IOException e){
-             System.err.println("[MineColony] " + e.getMessage()+" save,2");
-             System.err.flush();
-           }
-         }
+    @Override
+    public World getClientWorld()
+    {
+        return FMLClientHandler.instance().getClient().theWorld;
+    }
 }
