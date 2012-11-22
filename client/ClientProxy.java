decc1de05defa01463707a334a9a8d5fbd2034a7
lohikaarme <lohikaarme.hav@kolumbus.fi>
2012-11-22 22:20:35 +0200

    Update client/ClientProxy.java
    Made so that file is created and changes how root is find.
    MineColony.cfg is created at jar folder. I didn't have time to make 
    config folder.

--- client/ClientProxy.java
+++ client/ClientProxy.java
@@ -1,29 +1,73 @@
+// 
+// data is in a public static hash table called ConfigFile.settings
+//   ConfigFile.settings.getProperty("myNewBlockID");
+//   ConfigFile.settings.setProperty("myNewBlockID", "1500");
+//
+// config file location is ../config/MineColony.cfg
+//
 package client;
 
-import server.ServerProxy;
-import net.minecraftforge.client.MinecraftForgeClient;
-import cpw.mods.fml.client.FMLClientHandler;
-import net.minecraft.src.World;
-import net.minecraftforge.client.MinecraftForgeClient;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
-public class ClientProxy extends ServerProxy
-{
+import net.minecraft.client.Minecraft;
 
-	@Override
-    public void registerRenderInformation()
-    {
-        MinecraftForgeClient.preloadTexture("/minecolony.png");
-    }
+public class ConfigFile {
+	
+	public static Properties settings = new Properties();
+	private static String configFilePath="MineColony.cfg";
+			
+	public static void createDefaultConfiguration() {
+	   // Default key/value pairs in ConfigFile.settings
+	   settings.setProperty("MineColony", "Reboot");
 
-    @Override
-    public void registerTileEntitySpecialRenderer(/*PLACEHOLDER*/)
-    {
-       
-    }
+	   //test purposes
+	   settings.setProperty("townhallID","1000");
+	   //
 
-    @Override
-    public World getClientWorld()
-    {
-        return FMLClientHandler.instance().getClient().theWorld;
-    }
+           save();
+	}
+	
+	
+	public static void load() {
+	    File configFile = new File(configFilePath);
+	  
+	    try{
+    	      if(configFile.exists() ){
+    	        FileInputStream in = new FileInputStream(configFile);	
+    	        settings.load(in);
+    	      }
+    	      else {
+    	        createDefaultConfiguration();
+   	      }
+
+	   }
+	   catch (Exception e) {
+             System.err.println("[MineColony] " + e.getMessage());
+             System.err.flush();
+           }
+         }
+
+	
+         public static void save() {
+           File configFile = new File(configFilePath);
+           try{
+             configFile.createNewFile();
+             FileOutputStream out = new FileOutputStream(configFile);
+             settings.store(out, "MineColony configuration file");
+             load();
+           }
+           catch (FileNotFoundException e) {
+             System.err.println("[MineColony] " + e.getMessage()+" save,1");
+             System.err.flush();
+           }
+           catch (IOException e){
+             System.err.println("[MineColony] " + e.getMessage()+" save,2");
+             System.err.flush();
+           }
+         }
 }
