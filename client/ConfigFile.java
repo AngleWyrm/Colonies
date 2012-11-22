9bbf4b40bcf79d71512364559520426b9e3e32c8
AngleWyrm <anglewyrm@yahoo.com>
2012-11-22 03:59:35 -0800

    ConfigFile.settings is now a public key/value hash table.

--- client/ConfigFile.java
+++ client/ConfigFile.java
@@ -1,17 +1,43 @@
-// Create, read and write configuration file
-// stored in user.dir/../config/MineColony.cfg
+// 
+// data is in a public static hash table called ConfigFile.settings
+//   ConfigFile.settings.getProperty("myNewBlockID");
+//   ConfigFile.settings.setProperty("myNewBlockID", "1500");
+//
+// config file location is ../config/MineColony.cfg
+//
 package client;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
 
 public class ConfigFile {
-
+	public static Properties settings;
+	
 	public static void CreateDefaultConfig() {
-			String configFilePath;
-			configFilePath = File.separator +".." + File.separator + "config";
-			// test for and possibly create config folder
-			// test for and possibly create MineColony.cfg
-			// catch exceptions and post to stream
-			// failure to create means use defaults in-game
+	
+	     // Default key/value pairs in ConfigFile.settings
+	     settings.setProperty("testBlockID", "3500");
+
+	     // TODO: Store settings in config file
+	     Path configFilePath = FileSystems.getDefault().getPath(".."+File.separator+"config", "MineColony.cfg");
+
 	}
+	
+	
+	public static void LoadConfig() {
+	     Path configFilePath = FileSystems.getDefault().getPath(".."+File.separator+"config", "MineColony.cfg");
+	     if( Files.exists(configFilePath) ){
+	    	 // TODO: load configuration settings
+	     }
+	     else{
+	    	 CreateDefaultConfig();
+	     }
+	}
+
+
 }
