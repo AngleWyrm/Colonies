663b93d6ee294df8dedf5a33336a1ae601b2d1ba
Gerbil123 <jeffreymrussell@gmail.com>
2012-11-26 09:30:23 +0200

    Starting work on creating the new chests from scratch

---
+++ vector67/src/BlockColoniesChest.java
@@ -0,0 +1,386 @@
+package colonies.vector67.src;
+
+import static net.minecraftforge.common.ForgeDirection.DOWN;
+
+import java.util.Iterator;
+import java.util.Random;
+
+import colonies.anglewyrm.src.ClientProxy;
+import cpw.mods.fml.common.Side;
+import cpw.mods.fml.common.asm.SideOnly;
+import net.minecraft.src.AxisAlignedBB;
+import net.minecraft.src.BlockContainer;
+import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.EntityItem;
+import net.minecraft.src.EntityLiving;
+import net.minecraft.src.EntityOcelot;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.IBlockAccess;
+import net.minecraft.src.IInventory;
+import net.minecraft.src.InventoryLargeChest;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.Material;
+import net.minecraft.src.MathHelper;
+import net.minecraft.src.NBTTagCompound;
+import net.minecraft.src.TileEntity;
+import net.minecraft.src.TileEntityChest;
+import net.minecraft.src.World;
+
+public class BlockColoniesChest extends BlockContainer {
+
+	private Random random = new Random();
+	public BlockColoniesChest(int id){
+		super(id, Material.wood);
+		setBlockName("ColoniesChest");
+		setHardness(1.0F);
+	    setRequiresSelfNotify();
+	    setBlockBounds(0.0625F, 0F, 0.0625F, 0.9375F, 0.875F, 0.9375F);
+	    setCreativeTab(CreativeTabs.tabDecorations);
+	}
+	/**
+     * Is this block (a) opaque and (b) a full 1m cube?  This determines whether or not to render the shared face of two
+     * adjacent blocks and also whether the player can attach torches, redstone wire, etc to this block.
+     */
+    public boolean isOpaqueCube()
+    {
+        return false;
+    }
+
+    /**
+     * If this block doesn't render as an ordinary block it will return False (examples: signs, buttons, stairs, etc)
+     */
+    public boolean renderAsNormalBlock()
+    {
+        return false;
+    }
+
+    /**
+     * The type of render function that is called for this block
+     */
+    public int getRenderType()
+    {
+        return 22;
+    }
+
+    /**
+     * Updates the blocks bounds based on its current state. Args: world, x, y, z
+     */
+    public void setBlockBoundsBasedOnState(IBlockAccess par1IBlockAccess, int par2, int par3, int par4)
+    {
+        if (par1IBlockAccess.getBlockId(par2, par3, par4 - 1) == this.blockID)
+        {
+            this.setBlockBounds(0.0625F, 0.0F, 0.0F, 0.9375F, 0.875F, 0.9375F);
+        }
+        else if (par1IBlockAccess.getBlockId(par2, par3, par4 + 1) == this.blockID)
+        {
+            this.setBlockBounds(0.0625F, 0.0F, 0.0625F, 0.9375F, 0.875F, 1.0F);
+        }
+        else if (par1IBlockAccess.getBlockId(par2 - 1, par3, par4) == this.blockID)
+        {
+            this.setBlockBounds(0.0F, 0.0F, 0.0625F, 0.9375F, 0.875F, 0.9375F);
+        }
+        else if (par1IBlockAccess.getBlockId(par2 + 1, par3, par4) == this.blockID)
+        {
+            this.setBlockBounds(0.0625F, 0.0F, 0.0625F, 1.0F, 0.875F, 0.9375F);
+        }
+        else
+        {
+            this.setBlockBounds(0.0625F, 0.0F, 0.0625F, 0.9375F, 0.875F, 0.9375F);
+        }
+    }
+
+    /**
+     * Called whenever the block is added into the world. Args: world, x, y, z
+     */
+    public void onBlockAdded(World par1World, int par2, int par3, int par4)
+    {
+        super.onBlockAdded(par1World, par2, par3, par4);
+        int var5 = par1World.getBlockId(par2, par3, par4 - 1);
+        int var6 = par1World.getBlockId(par2, par3, par4 + 1);
+        int var7 = par1World.getBlockId(par2 - 1, par3, par4);
+        int var8 = par1World.getBlockId(par2 + 1, par3, par4);
+    }
+
+    /**
+     * Called when the block is placed in the world.
+     */
+    public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLiving par5EntityLiving)
+    {
+        int var6 = par1World.getBlockId(par2, par3, par4 - 1);
+        int var7 = par1World.getBlockId(par2, par3, par4 + 1);
+        int var8 = par1World.getBlockId(par2 - 1, par3, par4);
+        int var9 = par1World.getBlockId(par2 + 1, par3, par4);
+        byte var10 = 0;
+        int var11 = MathHelper.floor_double((double)(par5EntityLiving.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+        if (var11 == 0)
+        {
+            var10 = 2;
+        }
+
+        if (var11 == 1)
+        {
+            var10 = 5;
+        }
+
+        if (var11 == 2)
+        {
+            var10 = 3;
+        }
+
+        if (var11 == 3)
+        {
+            var10 = 4;
+        }
+
+        if (var6 != this.blockID && var7 != this.blockID && var8 != this.blockID && var9 != this.blockID)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, var10);
+        }
+        else
+        {
+            if ((var6 == this.blockID || var7 == this.blockID) && (var10 == 4 || var10 == 5))
+            {
+                if (var6 == this.blockID)
+                {
+                    par1World.setBlockMetadataWithNotify(par2, par3, par4 - 1, var10);
+                }
+                else
+                {
+                    par1World.setBlockMetadataWithNotify(par2, par3, par4 + 1, var10);
+                }
+
+                par1World.setBlockMetadataWithNotify(par2, par3, par4, var10);
+            }
+
+            if ((var8 == this.blockID || var9 == this.blockID) && (var10 == 2 || var10 == 3))
+            {
+                if (var8 == this.blockID)
+                {
+                    par1World.setBlockMetadataWithNotify(par2 - 1, par3, par4, var10);
+                }
+                else
+                {
+                    par1World.setBlockMetadataWithNotify(par2 + 1, par3, par4, var10);
+                }
+
+                par1World.setBlockMetadataWithNotify(par2, par3, par4, var10);
+            }
+        }
+    }
+    @SideOnly(Side.CLIENT)
+
+    /**
+     * Retrieves the block texture to use based on the display side. Args: iBlockAccess, x, y, z, side
+     */
+    public int getBlockTexture(IBlockAccess par1IBlockAccess, int par2, int par3, int par4, int par5)
+    {
+        return 0;
+    }
+
+    /**
+     * Returns the block texture based on the side being looked at.  Args: side
+     */
+    public int getBlockTextureFromSide(int par1)
+    {
+        return 4;
+    }
+
+    /**
+     * Checks to see if its valid to put this block at the specified coordinates. Args: world, x, y, z
+     */
+    public boolean canPlaceBlockAt(World par1World, int par2, int par3, int par4)
+    {
+        int var5 = 0;
+
+        if (par1World.getBlockId(par2 - 1, par3, par4) == this.blockID)
+        {
+            ++var5;
+        }
+
+        if (par1World.getBlockId(par2 + 1, par3, par4) == this.blockID)
+        {
+            ++var5;
+        }
+
+        if (par1World.getBlockId(par2, par3, par4 - 1) == this.blockID)
+        {
+            ++var5;
+        }
+
+        if (par1World.getBlockId(par2, par3, par4 + 1) == this.blockID)
+        {
+            ++var5;
+        }
+
+        return var5 > 1 ? false : (this.isThereANeighborChest(par1World, par2 - 1, par3, par4) ? false : (this.isThereANeighborChest(par1World, par2 + 1, par3, par4) ? false : (this.isThereANeighborChest(par1World, par2, par3, par4 - 1) ? false : !this.isThereANeighborChest(par1World, par2, par3, par4 + 1))));
+    }
+    private boolean isThereANeighborChest(World par1World, int par2, int par3, int par4)
+    {
+        return par1World.getBlockId(par2, par3, par4) != this.blockID ? false : (par1World.getBlockId(par2 - 1, par3, par4) == this.blockID ? true : (par1World.getBlockId(par2 + 1, par3, par4) == this.blockID ? true : (par1World.getBlockId(par2, par3, par4 - 1) == this.blockID ? true : par1World.getBlockId(par2, par3, par4 + 1) == this.blockID)));
+    }
+    /**
+     * ejects contained items into the world, and notifies neighbours of an update, as appropriate
+     */
+    public void breakBlock(World par1World, int par2, int par3, int par4, int par5, int par6)
+    {
+        TileEntityChest var7 = (TileEntityChest)par1World.getBlockTileEntity(par2, par3, par4);
+
+        if (var7 != null)
+        {
+            for (int var8 = 0; var8 < var7.getSizeInventory(); ++var8)
+            {
+                ItemStack var9 = var7.getStackInSlot(var8);
+
+                if (var9 != null)
+                {
+                    float var10 = this.random.nextFloat() * 0.8F + 0.1F;
+                    float var11 = this.random.nextFloat() * 0.8F + 0.1F;
+                    EntityItem var14;
+
+                    for (float var12 = this.random.nextFloat() * 0.8F + 0.1F; var9.stackSize > 0; par1World.spawnEntityInWorld(var14))
+                    {
+                        int var13 = this.random.nextInt(21) + 10;
+
+                        if (var13 > var9.stackSize)
+                        {
+                            var13 = var9.stackSize;
+                        }
+
+                        var9.stackSize -= var13;
+                        var14 = new EntityItem(par1World, (double)((float)par2 + var10), (double)((float)par3 + var11), (double)((float)par4 + var12), new ItemStack(var9.itemID, var13, var9.getItemDamage()));
+                        float var15 = 0.05F;
+                        var14.motionX = (double)((float)this.random.nextGaussian() * var15);
+                        var14.motionY = (double)((float)this.random.nextGaussian() * var15 + 0.2F);
+                        var14.motionZ = (double)((float)this.random.nextGaussian() * var15);
+
+                        if (var9.hasTagCompound())
+                        {
+                            var14.item.setTagCompound((NBTTagCompound)var9.getTagCompound().copy());
+                        }
+                    }
+                }
+            }
+        }
+
+        super.breakBlock(par1World, par2, par3, par4, par5, par6);
+    }
+
+    /**
+     * Called upon block activation (right click on the block.)
+     */
+    public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
+    {
+        Object var10 = (TileEntityChest)par1World.getBlockTileEntity(par2, par3, par4);
+
+        if (var10 == null)
+        {
+            return true;
+        }
+        else if (par1World.isBlockSolidOnSide(par2, par3 + 1, par4, DOWN))
+        {
+            return true;
+        }
+        else if (isOcelotBlockingChest(par1World, par2, par3, par4))
+        {
+            return true;
+        }
+        else if (par1World.getBlockId(par2 - 1, par3, par4) == this.blockID && (par1World.isBlockSolidOnSide(par2 - 1, par3 + 1, par4, DOWN) || isOcelotBlockingChest(par1World, par2 - 1, par3, par4)))
+        {
+            return true;
+        }
+        else if (par1World.getBlockId(par2 + 1, par3, par4) == this.blockID && (par1World.isBlockSolidOnSide(par2 + 1, par3 + 1, par4, DOWN) || isOcelotBlockingChest(par1World, par2 + 1, par3, par4)))
+        {
+            return true;
+        }
+        else if (par1World.getBlockId(par2, par3, par4 - 1) == this.blockID && (par1World.isBlockSolidOnSide(par2, par3 + 1, par4 - 1, DOWN) || isOcelotBlockingChest(par1World, par2, par3, par4 - 1)))
+        {
+            return true;
+        }
+        else if (par1World.getBlockId(par2, par3, par4 + 1) == this.blockID && (par1World.isBlockSolidOnSide(par2, par3 + 1, par4 + 1, DOWN) || isOcelotBlockingChest(par1World, par2, par3, par4 + 1)))
+        {
+            return true;
+        }
+        else
+        {
+            if (par1World.getBlockId(par2 - 1, par3, par4) == this.blockID)
+            {
+                var10 = new InventoryLargeChest("container.chestDouble", (TileEntityChest)par1World.getBlockTileEntity(par2 - 1, par3, par4), (IInventory)var10);
+            }
+
+            if (par1World.getBlockId(par2 + 1, par3, par4) == this.blockID)
+            {
+                var10 = new InventoryLargeChest("container.chestDouble", (IInventory)var10, (TileEntityChest)par1World.getBlockTileEntity(par2 + 1, par3, par4));
+            }
+
+            if (par1World.getBlockId(par2, par3, par4 - 1) == this.blockID)
+            {
+                var10 = new InventoryLargeChest("container.chestDouble", (TileEntityChest)par1World.getBlockTileEntity(par2, par3, par4 - 1), (IInventory)var10);
+            }
+
+            if (par1World.getBlockId(par2, par3, par4 + 1) == this.blockID)
+            {
+                var10 = new InventoryLargeChest("container.chestDouble", (IInventory)var10, (TileEntityChest)par1World.getBlockTileEntity(par2, par3, par4 + 1));
+            }
+
+            if (par1World.isRemote)
+            {
+                return true;
+            }
+            else
+            {
+                par5EntityPlayer.displayGUIChest((IInventory)var10);
+                return true;
+            }
+        }
+    }
+
+    /**
+     * Returns a new instance of a block's tile entity class. Called on placing the block.
+     */
+    public TileEntity createNewTileEntity(World par1World)
+    {
+        return new TileEntityChest();
+    }
+
+    /**
+     * Looks for a sitting ocelot within certain bounds. Such an ocelot is considered to be blocking access to the
+     * chest.
+     */
+    public static boolean isOcelotBlockingChest(World par0World, int par1, int par2, int par3)
+    {
+        Iterator var4 = par0World.getEntitiesWithinAABB(EntityOcelot.class, AxisAlignedBB.getAABBPool().addOrModifyAABBInPool((double)par1, (double)(par2 + 1), (double)par3, (double)(par1 + 1), (double)(par2 + 2), (double)(par3 + 1))).iterator();
+        EntityOcelot var6;
+
+        do
+        {
+            if (!var4.hasNext())
+            {
+                return false;
+            }
+
+            EntityOcelot var5 = (EntityOcelot)var4.next();
+            var6 = (EntityOcelot)var5;
+        }
+        while (!var6.isSitting());
+
+        return true;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	 @Override
+	  public String getTextureFile() {
+	    return ClientProxy.CHESTCONTAINER_PNG;//"/cpw/mods/ironchest/sprites/block_textures.png";
+	  }
+
+}
--- vector67/src/DeleteMe.java
+++
@@ -1,8 +0,0 @@
-// This stub is just to generate the directory
-// It can go away now. :D
-
-package colonies.vector67.src;
-
-public class DeleteMe {
-
-}
