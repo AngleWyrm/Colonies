845edcc9ab3fd512b554b295b94027a1ab46aafd
Gerbil123 <jeffreymrussell@gmail.com>
2012-11-26 09:46:51 +0200

    Had to add texture file references.

--- anglewyrm/src/ClientProxy.java
+++ anglewyrm/src/ClientProxy.java
@@ -7,7 +7,15 @@ import cpw.mods.fml.client.FMLClientHandler;
 
 public class ClientProxy extends ServerProxy
 {
-
+	
+	public static String BLACKSMITHCHEST_PNG = "/minecraft/colonies/Minecraft/bin/cpw/mods/Colonies/Chests/Blacksmithchest.png";
+	public static String BUILDERCHEST_PNG = "/minecraft/colonies/Minecraft/bin/cpw/mods/Colonies/Chests/Builderchest.png";
+	public static String FARMERCHEST_PNG = "/minecraft/colonies/Minecraft/bin/cpw/mods/Colonies/Chests/Farmerchest.png";
+	public static String HOUSECHEST_PNG = "/minecraft/colonies/Minecraft/bin/cpw/mods/Colonies/Chests/Housechest.png";
+	public static String LUMBERJACKCHEST_PNG = "/minecraft/colonies/Minecraft/bin/cpw/mods/Colonies/Chests/Lumberjackchest.png";
+	public static String MINERCHEST_PNG = "/minecraft/colonies/Minecraft/bin/cpw/mods/Colonies/Chests/Minerchest.png";
+	public static String TOWNHALLCHEST_PNG = "/colonies/Minecraft/bin/cpw/mods/Colonies/Chests/Townhallchest.png";
+	public static String CHESTCONTAINER_PNG = "/colonies/pmardle/gfx/Chestcontainer.png";
 	@Override
     public void registerRenderInformation()
     {
