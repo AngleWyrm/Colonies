6be3156e0e10b2e2463f09aff03f2819e0e9153e
lohikaarme <lohikaarme.HAV@gmail.com>
2012-12-09 12:42:45 +0200

    Fix to the chest drawing

--- vector67/src/ColoniesChestRenderHelper.java
+++ vector67/src/ColoniesChestRenderHelper.java
@@ -11,15 +11,18 @@ import net.minecraft.src.ChestItemRenderHelper;
 import net.minecraft.src.TileEntityChest;
 import net.minecraft.src.TileEntityRenderer;
 
-public class ColoniesChestRenderHelper extends ChestItemRenderHelper {
+public class ColoniesChestRenderHelper extends ChestItemRenderHelper{
+		
 	private Map<Integer, TileEntityColoniesChest> itemRenders = Maps.newHashMap();
 
 	public ColoniesChestRenderHelper() {
 		itemRenders.put(0, (TileEntityColoniesChest) ColoniesMain.chestBlock.createTileEntity(null, 0));
-		
 	}
-	@Override
+	
 	public void renderChest(Block block, int i, float f) {
-		TileEntityRenderer.instance.renderTileEntityAt(itemRenders.get(i), 0.0D, 0.0D, 0.0D, 0.0F);
+		super.renderChest(block,i,f);
+		if(block.blockID==ColoniesMain.chestBlock.blockID){
+		  TileEntityRenderer.instance.renderTileEntityAt(itemRenders.get(i), 0.0D, 0.0D, 0.0D, 0.0F);
+		}
 	}
 }
