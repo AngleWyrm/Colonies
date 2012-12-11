f1f26be1346dd8433686f7dac6fddebb5d82fe21
AngleWyrm <anglewyrm@yahoo.com>
2012-12-10 17:45:53 -0800

    Switched to .ogg format to reduce memory footprint

--- anglewyrm/src/ColoniesSoundManager.java
+++ anglewyrm/src/ColoniesSoundManager.java
@@ -13,13 +13,13 @@ import net.minecraftforge.event.ForgeSubscribe;
 	    {
 	        try
 	        {
-	        	event.manager.soundPoolSounds.addSound("colonies/m-hello.wav", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/m-hello.wav"));
-	        	event.manager.soundPoolSounds.addSound("colonies/f-hello.wav", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/f-hello.wav"));            
-	        	event.manager.soundPoolSounds.addSound("colonies/m-hurt1.wav", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/m-hurt1.wav"));
-	        	event.manager.soundPoolSounds.addSound("colonies/m-hurt2.wav", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/m-hurt2.wav"));
-	        	event.manager.soundPoolSounds.addSound("colonies/m-hurt3.wav", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/m-hurt3.wav"));
-	            event.manager.soundPoolSounds.addSound("colonies/f-damnit.wav", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/f-damnit.wav"));            
-	            event.manager.soundPoolSounds.addSound("colonies/f-ohyeah.wav", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/f-ohyeah.wav"));            
+	        	event.manager.soundPoolSounds.addSound("colonies/m-hello.ogg", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/m-hello.ogg"));
+	        	event.manager.soundPoolSounds.addSound("colonies/f-hello.ogg", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/f-hello.ogg"));            
+	        	event.manager.soundPoolSounds.addSound("colonies/m-hurt1.ogg", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/m-hurt1.ogg"));
+	        	event.manager.soundPoolSounds.addSound("colonies/m-hurt2.ogg", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/m-hurt2.ogg"));
+	        	event.manager.soundPoolSounds.addSound("colonies/m-hurt3.ogg", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/m-hurt3.ogg"));
+	            event.manager.soundPoolSounds.addSound("colonies/f-damnit.ogg", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/f-damnit.ogg"));            
+	            event.manager.soundPoolSounds.addSound("colonies/f-ohyeah.ogg", ColoniesMain.class.getResource("/colonies/anglewyrm/snd/f-ohyeah.ogg"));            
 	       
 	        }
 	        catch (Exception e)
