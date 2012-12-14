a5d4ebdcfba42ae1f64be0f8e5696726dfe6f1cb
AngleWyrm <anglewyrm@yahoo.com>
2012-12-13 22:38:39 -0800

    filename correction

--- anglewyrm/src/ServerProxy.java
+++ anglewyrm/src/ServerProxy.java
@@ -21,7 +21,7 @@ public class ServerProxy implements IGuiHandler
 	public static String HOUSECHEST_PNG      = "/colonies/pmardle/gfx/Housechest.png";
 	public static String LOGGINGCAMP_PNG     = "/colonies/pmardle/gfx/Lumberjackchest.png";
 	public static String MINERCHEST_PNG      = "/colonies/pmardle/gfx/Minerchest.png";
-	public static String TOWNHALLCHEST_PNG   = "/colonies/pmardle/gfx/Townhallchest.png";
+	public static String TOWNHALLCHEST_PNG   = "/colonies/pmardle/gfx/TownhallChest.png";
 	public static String CHESTCONTAINER_PNG  = "/colonies/anglewyrm/gfx/invalidChest.png";
 	public static String MEASURING_TAPE      = "/colonies/lohikaarme/gfx/LItems.png";
 	
