ff080873b781130ac0c8dccd745baccb555b81a8
Kaiden <Kaiden@ZEUS>
2012-12-22 09:24:27 +0800

    Test

--- kzolp67/src/ItemCalculator.java
+++ kzolp67/src/ItemCalculator.java
@@ -1,11 +1,13 @@
 package colonies.kzolp67.src;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.src.CreativeTabs;
 import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
 import net.minecraft.src.World;
 import colonies.anglewyrm.src.ServerProxy;
+import colonies.anglewyrm.src.Utility;
 
 public class ItemCalculator extends Item{
   
@@ -16,6 +18,14 @@ public class ItemCalculator extends Item{
 		setIconCoord(14,0);
 		setCreativeTab(CreativeTabs.tabTools);
 	}
+	
+
+    public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer player, World world, int x, int y, int z, int par7, float par8, float par9, float par10){
+		       
+    	Utility.chatMessage("Test");
+		return true; 
+	       
+    }
     
 	@Override
 	public String getTextureFile(){
