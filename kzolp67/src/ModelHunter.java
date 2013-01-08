9e3264a51ed24ce2477ed207d4666bf0b27dd512
Kaiden <Kaiden@ZEUS>
2013-01-08 16:17:06 +0800

    Fixed the hunter model

--- kzolp67/src/ModelHunter.java
+++ kzolp67/src/ModelHunter.java
@@ -1,14 +1,12 @@
 package colonies.kzolp67.src;
 
 import net.minecraft.src.Entity;
-import net.minecraft.src.MathHelper;
 import net.minecraft.src.ModelBase;
 import net.minecraft.src.ModelRenderer;
 
 public class ModelHunter extends ModelBase
 {
   //fields
-    ModelRenderer hair;
     ModelRenderer head;
     ModelRenderer body;
     ModelRenderer rightarm;
@@ -18,49 +16,43 @@ public class ModelHunter extends ModelBase
   
   public ModelHunter()
   {
-    textureWidth = 128;
-    textureHeight = 64;
+    textureWidth = 64;
+    textureHeight = 32;
     
-      hair = new ModelRenderer(this, 0, 32);
-      hair.addBox(0F, 0F, 0F, 10, 10, 10);
-      hair.setRotationPoint(-5F, -9F, -5F);
-      hair.setTextureSize(128, 64);
-      hair.mirror = true;
-      setRotation(hair, 0F, 0F, 0F);
       head = new ModelRenderer(this, 0, 0);
       head.addBox(-4F, -8F, -4F, 8, 8, 8);
       head.setRotationPoint(0F, 0F, 0F);
-      head.setTextureSize(128, 64);
+      head.setTextureSize(64, 32);
       head.mirror = true;
       setRotation(head, 0F, 0F, 0F);
       body = new ModelRenderer(this, 16, 16);
       body.addBox(-4F, 0F, -2F, 8, 12, 4);
       body.setRotationPoint(0F, 0F, 0F);
-      body.setTextureSize(128, 64);
+      body.setTextureSize(64, 32);
       body.mirror = true;
       setRotation(body, 0F, 0F, 0F);
       rightarm = new ModelRenderer(this, 40, 16);
       rightarm.addBox(-3F, -2F, -2F, 4, 12, 4);
       rightarm.setRotationPoint(-5F, 2F, 0F);
-      rightarm.setTextureSize(128, 64);
+      rightarm.setTextureSize(64, 32);
       rightarm.mirror = true;
       setRotation(rightarm, 0F, 0F, 0F);
       leftarm = new ModelRenderer(this, 40, 16);
       leftarm.addBox(-1F, -2F, -2F, 4, 12, 4);
       leftarm.setRotationPoint(5F, 2F, 0F);
-      leftarm.setTextureSize(128, 64);
+      leftarm.setTextureSize(64, 32);
       leftarm.mirror = true;
       setRotation(leftarm, 0F, 0F, 0F);
       rightleg = new ModelRenderer(this, 0, 16);
       rightleg.addBox(-2F, 0F, -2F, 4, 12, 4);
       rightleg.setRotationPoint(-2F, 12F, 0F);
-      rightleg.setTextureSize(128, 64);
+      rightleg.setTextureSize(64, 32);
       rightleg.mirror = true;
       setRotation(rightleg, 0F, 0F, 0F);
       leftleg = new ModelRenderer(this, 0, 16);
       leftleg.addBox(-2F, 0F, -2F, 4, 12, 4);
       leftleg.setRotationPoint(2F, 12F, 0F);
-      leftleg.setTextureSize(128, 64);
+      leftleg.setTextureSize(64, 32);
       leftleg.mirror = true;
       setRotation(leftleg, 0F, 0F, 0F);
   }
@@ -69,7 +61,6 @@ public class ModelHunter extends ModelBase
   {
     super.render(entity, f, f1, f2, f3, f4, f5);
     setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-    hair.render(f5);
     head.render(f5);
     body.render(f5);
     rightarm.render(f5);
@@ -87,13 +78,7 @@ public class ModelHunter extends ModelBase
   
   public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity me)
   {
-	   super.setRotationAngles(f, f1, f2, f3, f4, f5, me);
-	   
-	   // Model animation calculations
-	   rightleg.rotateAngleX = MathHelper.cos(f * 0.6662F) * 1.4F * f1;
-	   leftleg.rotateAngleX = MathHelper.cos(f * 0.6662F + 3.1415927F) * 1.4F * f1;
-	   leftarm.rotateAngleX = MathHelper.cos(f * 0.6662F) * 1.4F * f1;
-	   rightarm.rotateAngleX = MathHelper.cos(f * 0.6662F + 3.1415927F) * 1.4F * f1;
+    super.setRotationAngles(f, f1, f2, f3, f4, f5, me);
   }
 
 }
