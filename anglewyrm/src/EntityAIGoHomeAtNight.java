07797debbc3a5bed3e7666fa87c96d54a21a02da
AngleWyrm <anglewyrm@yahoo.com>
2013-01-12 05:24:30 -0800

    #68 Citizens go home at night

---
+++ anglewyrm/src/EntityAIGoHomeAtNight.java
@@ -0,0 +1,82 @@
+package colonies.anglewyrm.src;
+//
+// TODO: Possibly add a navigate to town hall phase
+//
+import java.lang.reflect.Array;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Vector;
+import colonies.vector67.src.BlockColoniesChest;
+import colonies.anglewyrm.src.TileEntityTownHall;
+import net.minecraft.client.Minecraft;
+import net.minecraft.src.EntityAIBase;
+import net.minecraft.src.ModLoader;
+import colonies.vector67.src.TileEntityColoniesChest;
+
+// Join a town
+public class EntityAIGoHomeAtNight extends EntityAIBase 
+{
+	public static double TOO_FAR_AWAY = 30;
+	private EntityCitizen citizen;
+	private TileEntityColoniesChest destination;
+	
+
+	public EntityAIGoHomeAtNight(EntityCitizen _citizen) {
+		this.citizen = _citizen;
+		this.setMutexBits(1); 
+	}
+
+	@Override
+	public boolean shouldExecute() 
+	{
+		// reasons to idle this task in the background
+		if( citizen.worldObj.isDaytime() ) return false;
+		if( TileEntityTownHall.playerTown == null ) return false;
+
+		return true;
+	}
+	
+    public void startExecuting()
+    {
+    	if(TileEntityTownHall.playerTown == null) return;
+    	
+    	Utility.Debug("Going home for the night");   	
+   	   	// select destination
+    	if(citizen.residence != null){
+    		destination = citizen.residence;
+    	}
+    	else{
+    		destination = TileEntityTownHall.playerTown;
+    	}
+    	   	
+   		this.citizen.getNavigator()
+   			.tryMoveToXYZ(destination.xCoord, destination.yCoord, destination.zCoord, 0.25f);
+    }
+
+    public boolean continueExecuting()
+    {
+    	if(destination == null) return false;
+    	
+    	if(distanceToBlock(destination) < 4d){
+			Utility.Debug("Bed time");
+			// Minecraft.getMinecraft().thePlayer.addChatMessage("Sleeping");    			
+			return false;
+    	}
+
+    	boolean pathOK = !this.citizen.getNavigator().noPath();
+    	if(pathOK){
+    		Utility.Debug("...I can see it from here!");
+    	}
+    	else{
+    		Utility.Debug("Oh no! I can't get there from here!");
+    	}
+    	return pathOK;
+    }
+   
+	private double distanceToBlock(TileEntityColoniesChest tile){
+		if(tile==null) return 999;
+		double distance = tile.getDistanceFrom(citizen.posX, citizen.posY, citizen.posZ);
+		return Math.sqrt(distance);
+	}
+}
--- anglewyrm/src/EntityCitizen.java
+++ anglewyrm/src/EntityCitizen.java
@@ -70,8 +70,9 @@ public class EntityCitizen extends EntityCreature implements IMob // TODO: Make 
 	    this.tasks.addTask(3, new EntityAIMaintainInventoryLevels(this));
 	    this.tasks.addTask(4, new EntityAIJoinTown(this));
 		this.tasks.addTask(5, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+		this.tasks.addTask(5, new EntityAIGoHomeAtNight(this));
 	    this.tasks.addTask(6, new EntityAIWander(this, this.moveSpeed));
-	    this.tasks.addTask(7, new EntityAIVisit(this));
+	    this.tasks.addTask(7, new EntityAIGoHomeAtNight(this));
 
 	    this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));
 	    
