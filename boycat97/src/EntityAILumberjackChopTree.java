8722e37eed5fa2bb54ca69931cb0986df1c98ceb
boycat97 <boycat97@gmail.com>
2013-01-26 18:51:28 -0500

    Fixed chopping task to fit the continuous method more appropriately.

--- boycat97/src/EntityAILumberjackChopTree.java
+++ boycat97/src/EntityAILumberjackChopTree.java
@@ -14,77 +14,41 @@ import cpw.mods.fml.common.network.Player;
 
 public class EntityAILumberjackChopTree extends EntityAIGoToWork
 {
-	
-	private enum task {
-		chopping,
-		placingSapling,
-		walking
-	}
-	
-	private task currentTask;
 
 	public EntityAILumberjackChopTree(EntityLiving par1EntityLiving, int BlockID, float _movementSpeed) {
 		super(par1EntityLiving, BlockID, _movementSpeed);
-		this.currentTask = task.walking;
 		
 	}
 	
 	public boolean continueExecuting()
 	{
-		
-		if (this.currentTask.equals(task.chopping) && Math.abs(this.targetBlockX - this.taskEntity.posX) < 2) {
-			if (lookForWorkLocation() != null )
-			{
-				this.collectingWood();
-			} else {
-				this.currentTask = task.walking;
-			}
-			
-			return true;
-		}
-		
+		//if we are looking for work then find tree and start chopping
 		if (lookForWorkLocation() != null  && 
 		this.taskEntityWorld.getBlockId(MathHelper.floor_double(this.targetBlockX), MathHelper.floor_double(this.targetBlockY), MathHelper.floor_double(this.targetBlockZ)) == 17 ) 
 		{	
 			
-			this.currentTask = task.chopping;
-			
-			if ( Math.abs(this.targetBlockX - this.taskEntity.posX) < 2) {
+			//make sure the entity is close to the wood
+			if ( Math.abs(this.targetBlockX - this.taskEntity.posX) <= 2) {
 				this.collectingWood();
-				this.taskEntity.setCurrentItemOrArmor(0, new ItemStack(Item.axeWood, 1));
+				//this.taskEntity.setCurrentItemOrArmor(0, new ItemStack(Item.axeWood, 1));
 			}
 			
-			return true;
+			return false;
 			
-		} else {
-			
-			return super.continueExecuting();
-		}	
+		} 
 		
+		return true;
 	}
 	
 	private void collectingWood() 
 	{
-		this.currentTask = task.chopping;
-		Minecraft.getMinecraft().effectRenderer.addBlockDestroyEffects((int)this.targetBlockX, (int)this.targetBlockY, (int)this.targetBlockZ, 16, 16);
-		Block.wood.harvestBlock(this.taskEntityWorld, Minecraft.getMinecraft().thePlayer, MathHelper.floor_double(this.targetBlockX), MathHelper.floor_double(this.targetBlockY), MathHelper.floor_double(this.targetBlockZ), 0);
-		//Utility.chatMessage("Chopping some wood...");
-	}	
-	
-	@Override
-	public void startExecuting()
-	{
-		if (!this.currentTask.equals(task.chopping)) {
-		 super.startExecuting();
-		}
-	}
-	
-	
-	@Override
-	public boolean shouldExecute() {
 		
-	   return super.shouldExecute();
-        
-	} 
+		//show the animation of the block being hit.
+		Minecraft.getMinecraft().effectRenderer.addBlockDestroyEffects((int)this.targetBlockX, (int)this.targetBlockY, (int)this.targetBlockZ, 16, 16);
+		
+		//break a piece of wood off of the tree.
+		Block.wood.harvestBlock(this.taskEntityWorld, Minecraft.getMinecraft().thePlayer, MathHelper.floor_double(this.targetBlockX), MathHelper.floor_double(this.targetBlockY), MathHelper.floor_double(this.targetBlockZ), 0);
+	}	
+	 
 	
 }
