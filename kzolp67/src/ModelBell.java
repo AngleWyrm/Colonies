5e7c5fc41e2f163248d74f9d87e54d462d356ede
Kaiden <Kaiden@ZEUS>
2013-01-26 11:55:18 +0800

    Some work on #83

--- kzolp67/src/ModelBell.java
+++ kzolp67/src/ModelBell.java
@@ -223,7 +223,7 @@ public class ModelBell extends ModelBase
   public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
   {
     super.render(entity, f, f1, f2, f3, f4, f5);
-    setRotationAngles(f, f1, f2, f3, f4, f5);
+    setRotationAngles(f, f1, f2, f3, f4, f5, entity);
     Shape1.render(f5);
     Shape2.render(f5);
     Shape3.render(f5);
@@ -262,9 +262,9 @@ public class ModelBell extends ModelBase
     model.rotateAngleZ = z;
   }
   
-  public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5)
+  public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
   {
-    super.setRotationAngles(f, f1, f2, f3, f4, f5);
+    super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
   }
 
 }
