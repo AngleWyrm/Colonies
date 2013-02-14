1f23e8e63cc6e4a2c243fd9e63749f60eb19e6cb
AngleWyrm <anglewyrm@yahoo.com>
2013-02-13 17:00:03 -0800

    #100 planning sort function

--- anglewyrm/src/EntityAISeekEmployment.java
+++ anglewyrm/src/EntityAISeekEmployment.java
@@ -18,11 +18,18 @@ public class EntityAISeekEmployment extends EntityAIBase
 		if(TileEntityTownHall.playerTown.employersList == null) return false;
 		if(TileEntityTownHall.playerTown.employersList.isEmpty()) return false;
 		
-		// for now, fill first available job in employers list
-		EntityCitizen newJob = TileEntityTownHall.playerTown.getNextJob();
-		if(newJob != null){
-			citizen.setNewJob(newJob);
-		}
+		// TODO: if already employed, then usually skip this
+		if(citizen.employer != null) return false;
+		
+		return true;
+	}
+	
+	@Override
+	public boolean continueExecuting(){
+		
+		// assemble list of available jobs
+		// sort that list according to citizen's skill
+		// apply for jobs in list
 		
 		return false;
 	}
