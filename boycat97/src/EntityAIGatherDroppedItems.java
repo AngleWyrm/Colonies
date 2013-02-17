14306272ca7f39095d743f8e201ef0989b7c7f83
boycat97 <boycat97@gmail.com>
2013-02-16 23:27:02 -0500

    #99 Citizens now pick up items that are located within a vicinity of them.

---
+++ boycat97/src/EntityAIGatherDroppedItems.java
@@ -0,0 +1,89 @@
+package colonies.boycat97.src;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.src.Block;
+import net.minecraft.src.EntityAIBase;
+import net.minecraft.src.EntityItem;
+import net.minecraft.src.EntityLiving;
+import net.minecraft.src.IEntitySelector;
+import net.minecraft.src.Item;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.MathHelper;
+import net.minecraft.src.Vec3;
+import net.minecraft.src.World;
+import colonies.src.citizens.EntityCitizen;
+import colonies.src.citizens.EntityLumberjack;
+import colonies.src.Point;
+import colonies.src.Utility;
+import cpw.mods.fml.common.FMLModContainer;
+import cpw.mods.fml.common.network.Player;
+import net.minecraft.src.Entity;
+
+
+public class EntityAIGatherDroppedItems extends EntityAIBase 
+{
+	
+	private  EntityCitizen citizen;
+	private ItemStack itemsLookingFor;
+	private World taskEntityWorld;
+	private boolean foundItem;
+	
+	private final IEntitySelector field_82643_g;
+	
+	private Point destination;
+	
+	public EntityAIGatherDroppedItems(EntityCitizen _citizen)
+	{
+		this.setMutexBits(4);
+		this.citizen = _citizen;
+		this.foundItem = false;
+		this.field_82643_g = (IEntitySelector)null;
+	}
+
+	@Override
+	public boolean shouldExecute() {
+		
+		if(citizen == null) return false;
+		if(citizen.homeTown == null) return false; // must belong to a town
+		
+		return true;
+	}
+	
+	@Override 
+	public boolean continueExecuting()
+	{
+		//Look for item
+		List<EntityItem> foundItems = this.getNearbyEntities(taskEntityWorld, 12.0D);
+			
+		placeInInventory(foundItems);
+		return true;
+	}
+	
+	private List<EntityItem> getNearbyEntities(World world, double range) {	
+
+		List<EntityItem> items = citizen.worldObj.selectEntitiesWithinAABB(EntityItem.class, citizen.boundingBox.expand(range, range, range), this.field_82643_g);		
+		
+		return items;
+	}
+	
+	
+	private void placeInInventory(List<EntityItem> itemsToRetrieve)
+	{
+		//TODO: Determine what items they would like to have over just picking up everything.
+		for( EntityItem itemRetreived : itemsToRetrieve) {
+			if (this.citizen.inventory.addItemStackToInventory(itemRetreived.item) && itemRetreived.onGround ){
+				itemRetreived.setDead();
+			} else {
+				continue;
+			}
+		}
+		
+	}
+	
+	
+	
+}
--- src/citizens/EntityCitizen.java
+++ src/citizens/EntityCitizen.java
@@ -3,6 +3,7 @@ package colonies.src.citizens;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+import colonies.boycat97.src.EntityAIGatherDroppedItems;
 import colonies.src.ColoniesMain;
 import colonies.src.Utility;
 import colonies.src.buildings.TileEntityColoniesChest;
@@ -64,6 +65,7 @@ public class EntityCitizen extends EntityCreature implements IMob // TODO: Make 
 		this.tasks.addTask(0, new EntityAISwimming(this));
 		this.tasks.addTask(1, new EntityAIEatSomething(this));
 	    this.tasks.addTask(1, new EntityAIFindShelterFromRain(this, 0.4f));
+	    this.tasks.addTask(2, new EntityAIGatherDroppedItems(this));
 	    this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, this.moveSpeed, false));
 	    this.tasks.addTask(3, new EntityAIMaintainInventoryLevels(this));
 	    this.tasks.addTask(4, new EntityAIJoinTown(this));
